9318baa49ba263ba8f3a580525ee741d76902d96
awacloudcat <143956493+awacloudcat@users.noreply.github.com>
2023-09-05 00:20:14 +0800

    更新 test.py

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
 import time
-seale = 10
-print("执行开始".center(seale+2,"-"))
-start = time.perf_counter()
-for i in range(seale+1):
-    a = "*"*i
-    b = "."*(seale-i)
-    c = (i/seale)*100
-    dur = start = time.perf_counter()-start
-    print("\r{:<3.0f}%[{}->{}]{:.2f}s)".format(c,a,b,dur),end="")
-    time.sleep(1)
+start = time.time()
+time.sleep(1)
+stop = time.time()
+t = stop - start
+print(t)
