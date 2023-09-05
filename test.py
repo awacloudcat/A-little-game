6559401a467ff5bb655d969a62e4e584184ec541
awacloudcat <143956493+awacloudcat@users.noreply.github.com>
2023-09-05 17:21:20 +0800

    测试用，没写什么东西

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
-import time
-start = time.time()
-time.sleep(1)
-stop = time.time()
-t = stop - start
-print(t)
+from time import *
+from tkinter import *
+window = Tk()
+window.title("A game by playSgappy       ----仅供娱乐")
+window.resizable(0,0)
+time_start = time() #开始计时
+time_end = time()    #结束计时
+time_c= time_end - time_start   #运行所花时间
+print('time cost', time_c, 's')
+
+
+
+
+
+def start_lg():
+    global startTime
+    startTime = localtime().tm_sec
+    global lg
+    lg = Button(window,text='Stop',command=stop_lg)
+    lg.pack()
+def stop_lg():
+    endTime = localtime().tm_sec
+    sec = abs(endTime-startTime)%60
+    var1.set('%02s' %(sec))
+    lg.destroy()
+var1 = StringVar()
+lb1 = Label(window,textvariable=var1)
+lb1.pack()
